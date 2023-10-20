11af7516cd6c63a34a666f2d3ac397aa69b5b68d
DemetriosNikiforov <130399723+DemetriosNikiforov@users.noreply.github.com>
2023-10-20 17:36:06 +0300

    1.1.2

--- MarkerDima/Assets/Scripts/Payload.cs
+++ MarkerDima/Assets/Scripts/Payload.cs
@@ -1,6 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using Unity.Profiling;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Payload : MonoBehaviour, IMarkerUPNP
 {
@@ -8,6 +15,20 @@ public class Payload : MonoBehaviour, IMarkerUPNP
     public float RotationAngleY = 30f;
     [Header("Угол поворота ракетгицы:")]
     public float RotationAngleX = 30f;
+
+    public float SpeedRotationY = 0.1f;
+
+    public float SpeedRotationX = 0.1f;
+
+    [Header("Обьект полезной нагрузки для поворота по сои Y:")]
+    [SerializeField]
+    private Transform objectYAxisRotation;
+
+    [Header("Обьект полезной нагрузки для поворота по сои X:")]
+    [SerializeField]
+    private Transform objectXAxisRotation;
+
+
 
     [Header("Вес полезной нагрузки:")]
     [SerializeField]
@@ -26,6 +47,7 @@ public class Payload : MonoBehaviour, IMarkerUPNP
     }
 
 
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -33,8 +55,27 @@ public class Payload : MonoBehaviour, IMarkerUPNP
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+
+        float mouseX = Input.GetAxis("Mouse X");
+        if (Input.GetMouseButton(0))
+        {
+            //float angleY = ClampAngle(objectYAxisRotation.eulerAngles.y + , -30, 30);
+            //Debug.Log(angleY);
+
+            //objectYAxisRotation.localEulerAngles += Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * SpeedRotationY;
+
+            objectYAxisRotation.localEulerAngles += Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * SpeedRotationY;
+            objectYAxisRotation.eulerAngles = Vector3.up * ClampAngle(objectYAxisRotation.eulerAngles.y, 330, 30);
+            Debug.Log(objectYAxisRotation.eulerAngles.y);
+
+
+
+            //RotatePlayloadY();
+            //RotatePlayloadX();
+        }
 
     }
 
@@ -56,7 +97,85 @@ public class Payload : MonoBehaviour, IMarkerUPNP
     private void RotatePlayloadY()
     {
 
+        Vector3 targetPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+
+
+        targetPosition = Camera.main.ScreenToWorldPoint(targetPosition);
+
+
+
+
+        float mouseX = Input.GetAxis("Mouse X");
+        if (mouseX > 0)
+        {
+            Quaternion rotation = Quaternion.Euler(0, RotationAngleY, 0);
+
+
+            objectYAxisRotation.localRotation = Quaternion.Slerp(objectYAxisRotation.localRotation, rotation, SpeedRotationY);
+        }
+        else if (mouseX < 0)
+        {
+            Quaternion rotation = Quaternion.Euler(0, -RotationAngleY, 0);
+
+
+            objectYAxisRotation.localRotation = Quaternion.Slerp(objectYAxisRotation.localRotation, rotation, SpeedRotationY);
+        }
+
+
+
     }
 
+    private void RotatePlayloadX()
+    {
+        Vector3 targetPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+
+
+        targetPosition = Camera.main.ScreenToWorldPoint(targetPosition);
+
+
+
+
+        float mouseY = Input.GetAxis("Mouse Y");
+        if (mouseY > 0)
+        {
+            Quaternion rotation = Quaternion.Euler(-RotationAngleX, objectXAxisRotation.localRotation.y, objectXAxisRotation.localRotation.z);
+
+
+            objectXAxisRotation.localRotation = Quaternion.Slerp(objectYAxisRotation.localRotation, rotation, SpeedRotationX);
+        }
+        else if (mouseY < 0)
+        {
+            Quaternion rotation = Quaternion.Euler(RotationAngleX, objectXAxisRotation.localRotation.y, objectXAxisRotation.localRotation.z);
+
+
+            objectXAxisRotation.localRotation = Quaternion.Slerp(objectYAxisRotation.localRotation, rotation, SpeedRotationX);
+        }
+    }
+
+    private float ClampAngle(float value, float min, float max)
+    {
+
+        if ((min <= value && value<=360) || (0 < value && value <= max))
+        {
+            return value;
+
+        }
+        else if(value<min)
+        {
+            return min;
+        }else
+        {
+            return max;
+        }
+
+
+
+    }
 
 }
+
+
+
+
+
+
